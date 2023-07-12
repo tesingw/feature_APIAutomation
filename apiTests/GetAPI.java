c5cfe67eb40e2dd04f00bc356b49b0285f465191
Prasanth_Savaram <Prasanth_Savaram@epam.com>
2023-07-12 20:14:34 +0530

    Added automation Scenarios

---
+++ apiTests/GetAPI.java
@@ -0,0 +1,47 @@
+package apiTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GetAPI {
+
+	@Test(groups = "smoke")
+	public void testGetAPI() {
+		String endPoint = "library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI";
+		RestAssured.baseURI = "https://developer.apple.com/";
+		RequestSpecification request = RestAssured.given();
+		request.header("Accept", "application/json");
+		request.header("Content-Type", "application/json");
+		Response response = request.get(endPoint);
+		System.out.println(response.asString());
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+
+	@Test
+	public void testGetAPINegative() {
+		String endPoint = "library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI1";
+		RestAssured.baseURI = "https://developer.apple.com/";
+		RequestSpecification request = RestAssured.given();
+		request.header("Accept", "application/json");
+		request.header("Content-Type", "application/json");
+		Response response = request.get(endPoint);
+		System.out.println(response.asString());
+		Assert.assertEquals(response.getStatusCode(), 404);
+	}
+
+	@Test
+	public void testGetAPINoContent() {
+		String endPoint = "library/archive/documentation/AudioVideo/Conceptual";
+		RestAssured.baseURI = "https://developer.apple.com/";
+		RequestSpecification request = RestAssured.given();
+		request.header("Accept", "application/json");
+		request.header("Content-Type", "application/json");
+		Response response = request.get(endPoint);
+		System.out.println(response.asString());
+		Assert.assertEquals(response.getStatusCode(), 403);
+	}
+}
